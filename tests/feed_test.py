e0dc8515404fb3cd2a65dacd4a1f0c4684b5b11b
Mohammadreza Shaghouzi <sh.mohammad66@yahoo.com>
2023-07-19 22:30:56 +0330

    add response mock to unittest

---
+++ tests/feed_test.py
@@ -0,0 +1,22 @@
+from tests.base import TestBase
+
+from core import BSKY_BASE_URL, FEED_URL, BadRequestResponseException
+import requests_mock
+
+
+class FeedTest(TestBase):
+    def test_login_success(self):
+        with requests_mock.mock() as m:
+            m.get(f'{BSKY_BASE_URL}{FEED_URL}', text='{"feed":[]}')
+            result = self.client.get_author_feed('iooobp2.bsky.social')
+            self.assertEqual({"feed": []}, result)
+
+    def test_login_failed(self):
+        with requests_mock.mock() as m:
+            m.get(f'{BSKY_BASE_URL}{FEED_URL}',
+                  text='''{
+                            "error": "InvalidToken",
+                            "message": "Token could not be verified"
+                        }''', status_code=400)
+            self.assertRaises(
+                BadRequestResponseException, self.client.get_author_feed, 'iooobp2.bsky.social')
--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,12 +1,41 @@
 import unittest
 
-from core.utils import validate_value
+from core import validate_value, validate_get_response, BadRequestResponseException,AuthenticationRequiredException
+
+
+class MockResponse:
+    def __init__(self, json_data, status_code):
+        self.json_data = json_data
+        self.status_code = status_code
+
+    def json(self):
+        return self.json_data
 
 
 class UtilsTest(unittest.TestCase):
     def test_validate_value_success(self):
-        self.assertTrue(validate_value('salam',str))
+        self.assertTrue(validate_value('salam', str))
+
     def test_validate_value_failed(self):
-        self.assertRaises(TypeError, validate_value,'salam',int)
+        self.assertRaises(TypeError, validate_value, 'salam', int)
 
+    def test_response_200(self):
+        mock_response = MockResponse({"feed": []}, 200)
+        response = validate_get_response(mock_response)
+        self.assertEqual(response, {"feed": []})
 
+    def test_response_400(self):
+        mock_response = MockResponse({
+            "error": "InvalidToken",
+            "message": "Token could not be verified"
+        }, 400)
+        self.assertRaises(BadRequestResponseException,
+                          validate_get_response, mock_response)
+
+    def test_response_401(self):
+        mock_response = MockResponse({
+            "error": "AuthMissing",
+            "message": "Authentication Required"
+        }, 401)
+        self.assertRaises(AuthenticationRequiredException,
+                          validate_get_response, mock_response)
