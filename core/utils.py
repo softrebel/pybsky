8fb6f0048da1dd23d3d0e4f8d1af72b377efb4fb
Mohammadreza Shaghouzi <sh.mohammad66@yahoo.com>
2023-07-19 21:41:19 +0330

    add json encode and decode functions

--- core/utils.py
+++ core/utils.py
@@ -1,4 +1,15 @@
-def validate_value(value,cls):
-    if not isinstance(value,cls):
+import json
+
+
+def validate_value(value, cls):
+    if not isinstance(value, cls):
         raise TypeError(f'{value} must be in type {cls}')
     return True
+
+
+def json_decode(s: str) -> list | dict:
+    return json.loads(s)
+
+
+def json_encode(obj: object) -> str:
+    return json.dumps(obj, indent=4, ensure_ascii=False)
