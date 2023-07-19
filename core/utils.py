1459d5c7afd53cc67513c2159ce8dc69f5ff49df
Mohammadreza Shaghouzi <sh.mohammad66@yahoo.com>
2023-07-19 22:30:45 +0330

    add validate_get_response

--- core/utils.py
+++ core/utils.py
@@ -1,4 +1,7 @@
 import json
+import requests
+from .exceptions import AuthenticationRequiredException, UnknownResponseException, BadRequestResponseException
+Response = requests.models.Response
 
 
 def validate_value(value, cls):
@@ -13,3 +16,18 @@ def json_decode(s: str) -> list | dict:
 
 def json_encode(obj: object) -> str:
     return json.dumps(obj, indent=4, ensure_ascii=False)
+
+
+def validate_get_response(response: Response):
+    content = response.json()
+    if response.status_code == 200:
+        return content
+    elif response.status_code == 401:
+        raise AuthenticationRequiredException(content['message'])
+    elif response.status_code == 400:
+        raise BadRequestResponseException(content['message'])
+    else:
+        raise UnknownResponseException(
+            f"status code => {response.status_code} , response => {response.text}")
+
+    # TODO => handle all type of response
